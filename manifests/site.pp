9af587787a50fd9c5b8d0569f9a0505c7470ff56
Robson Gomes <nosbor84@gmail.com>
2019-04-16 07:33:55 -0300

    using hiera as node classifier

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,6 +25,8 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+hiera_include('classes')
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
